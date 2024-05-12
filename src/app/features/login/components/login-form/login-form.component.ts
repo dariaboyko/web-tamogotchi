4160b17673f89d45357b5e9b649b3c493e9bc18a
dariaboyko <dariaboiko03@gmail.com>
2024-05-12 12:25:22 +0300

    styles

--- src/app/features/login/components/login-form/login-form.component.ts
+++ src/app/features/login/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ILoginForm } from '@core';
 
@@ -6,6 +6,7 @@ import { ILoginForm } from '@core';
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginFormComponent {
   public loginForm: FormGroup<ILoginForm>;
